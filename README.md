778f517f6893e9485bf22d9a3d8ee266e2099ec5
rgowtham25 <85603627+rgowtham25@users.noreply.github.com>
2023-03-10 12:17:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ctuh6y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ctuh6y)
---
+++ src/app/tab-group-basic-example.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Basic use of the tab group
+ */
+@Component({
+  selector: 'tab-group-basic-example',
+  templateUrl: 'tab-group-basic-example.html',
+  styleUrls: ['tab-group-basic-example.css'],
+})
+export class TabGroupBasicExample {}
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
